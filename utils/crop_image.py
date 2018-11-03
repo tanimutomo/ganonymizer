d39f8d8f36e55fa7f2ead8b5d7ac02d64ec661ce
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2018-11-04 02:24:13 +0900

    change the structure of the directories

--- utils/crop_image.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread('../data/images/exp10.jpg')
-uly, ulx, rdy, rdx = 50, 50, 450, 450
-# uly, ulx, rdy, rdx = 340, 760, 740, 1160
-# uly, ulx, rdy, rdx = 56, 312, 456, 712
-img_crop = img[uly:rdy, ulx:rdx, :]
-cv2.imwrite('../data/images/exp10.png', img_crop)
