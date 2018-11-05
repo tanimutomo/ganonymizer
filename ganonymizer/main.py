b173d96fbbecac213002730bf7d22f9896a9730d
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2018-11-06 02:58:32 +0900

    commit violently

--- ganonymizer/main.py
+++ ganonymizer/main.py
@@ -10,10 +10,10 @@ def main():
 
 def get_config():
     config = {
-            'video': os.path.join(os.getcwd(), 'ganonymizer/data/videos/ex_small3_inter10_noon.avi'),
+            'video': os.path.join(os.getcwd(), 'ganonymizer/data/videos/ex_small2_inter10_noon.avi'),
             # The input image, when you apply GANonymizer to an image.
             'image': '', # os.path.join(os.getcwd(), 'ganonymizer/data/images/example_01.jpg'),
-            'output': '0',
+            'output': '1',
 
             # minimum probability to filter weak detections
             'conf': 0.5,
