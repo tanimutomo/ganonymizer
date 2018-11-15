6ebf979c0ce2422b1515a106d5d31e78e5435d67
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2018-11-15 23:46:20 +0900

    "auto-commit by kronos"

--- ganonymizer/main.py
+++ ganonymizer/main.py
@@ -13,7 +13,7 @@ def get_config():
             'video': '', # os.path.join(os.getcwd(), 'ganonymizer/data/videos/ex_small6_inter10_noon.avi'),
             # The input image, when you apply GANonymizer to an image.
             'image': os.path.join(os.getcwd(), 'ganonymizer/data/images/in_406.png'),
-            'output': '0',
+            'output': '1',
 
             'segmentation': False,
             # minimum probability to filter weak detections
