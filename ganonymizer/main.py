3281fd3b89384730dae9147ebf2be539574bd70a
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2018-12-23 23:58:26 +0900

    "auto-commit by kronos"

--- ganonymizer/main.py
+++ ganonymizer/main.py
@@ -68,7 +68,7 @@ def get_config():
             # the mask filename should be mask_{count_num}.png.
             # Note that this {count_num} start 1, not 0.
             # if you dont't use local masks, set None.
-            'use_local_masks': 'ganonymizer/data/videos/noon/large_mask',
+            'use_local_masks': 'ganonymizer/data/videos/noon/edge_mask',
 
 
             # For design of the output image or video
