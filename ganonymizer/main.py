0a20294b7d14c148bde640efa6cd5b07f7efb64b
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-03-15 12:22:19 +0900

    "auto-commit by kronos"

--- ganonymizer/main.py
+++ ganonymizer/main.py
@@ -12,7 +12,7 @@ def get_config():
     config = {
             'video': '', # os.path.join(os.getcwd(), 'ganonymizer/data/videos/ex_small6_inter10_noon.avi'),
             # The input image, when you apply GANonymizer to an image.
-            'image': os.path.join(os.getcwd(), 'ganonymizer/data/images/example_01.jpg'),
+            'image': os.path.join(os.getcwd(), 'ganonymizer/data/images/ex_02.png'),
             'output': '1',
 
             'segmentation': False,
