f75f29fbead8a3c2dea3d42e1a095a12d0da5548
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-05-06 01:21:04 +0900

    fix bugs and add bottom_left_crop funciton for corner esp test

--- src/utils/crop_img.py
+++ src/utils/crop_img.py
@@ -9,33 +9,46 @@ def imcrop(impath, pos, size):
         out = _left_crop(img, size)
     elif pos == 'center':
         out = _center_crop(img, size)
+    elif pos == 'bottom_left':
+        out = _bottom_left_crop(img, size)
     cv2.imwrite(impath, out)
 
 
 def _left_crop(img, size):
     h, w, _ = img.shape
-    tl_y = h / 2 - size / 2
+    tl_y = int(h / 2 - size / 2)
     tl_x = w - size
-    br_y = h - 1
-    br_x = w - 1
+    br_y = tl_y + size
+    br_x = w
     out = _crop_img(img, [tl_y, tl_x, br_y, br_x])
     return out
 
 
 def _center_crop(img, size):
     h, w, _ = img.shape
-    tl_y = h / 2 - size / 2
-    tl_x = w / 2 - size / 2
+    tl_y = int(h / 2 - size / 2)
+    tl_x = int(w / 2 - size / 2)
     br_y = tl_y + size
     br_x = tl_x + size
     out = _crop_img(img, [tl_y, tl_x, br_y, br_x])
     return out
 
 
+def _bottom_left_crop(img, size):
+    h, w, _ = img.shape
+    tl_y = h - size
+    tl_x = w - size
+    br_y = h
+    br_x = w
+    out = _crop_img(img, [tl_y, tl_x, br_y, br_x])
+    return out
+
+
 def _crop_img(img, rec):
-    return img[rec[1]:rec[3], rec[0]:rec[2]]
+    return img[rec[0]:rec[2], rec[1]:rec[3], :]
 
 
 def _load_img(impath):
     return cv2.imread(impath)
+
 
