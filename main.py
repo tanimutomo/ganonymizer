9de8c92beac5b9720613de428984306645c20893
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-05-06 01:18:45 +0900

    add the function to execute to dir and some param chantged

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 from src.executer import Executer
 from src.utils.util import Config
@@ -17,10 +18,23 @@ def v2f():
     out_dir = 'data/videos/noon/out_gfp_on'
     vid2frm(infile, out_dir)
 
+
 def main():
     config = Config(get_config())
     executer = Executer(config)
     executer.execute()
+
+
+def execute_to_dir():
+    config = Config(get_config())
+    config.det = 'data/noon_det_res'
+    config.boxline = 1
+    imlist = glob.glob('data/videos/noon/input/*.png')
+    for impath in imlist:
+        config.image = os.path.join(os.getcwd(), impath)
+        executer = Executer(config)
+        executer.execute()
+
 
 def create_exp_figs(crop=False):
     esp_px_config = Config(get_config())
@@ -29,7 +43,7 @@ def create_exp_figs(crop=False):
         esp_px_config.output = 'esp_px_{}'.format(name)
         esp_px_config.prepad_px = name
         esp_px_config.edge_mask = [0, 1, 120]
-        esp_px_config.det = 'data/experiment_fig_with_line'
+        esp_px_config.det = 'data/white_cropped'
         executer = Executer(esp_px_config)
         executer.execute()
         if crop:
@@ -49,7 +63,7 @@ def create_exp_figs(crop=False):
             gfp_div_config.pmd_div_num = num
             gfp_div_config.lattice_width = wid
             gfp_div_config.center_mask = 300
-            gfp_div_config.det = 'data/experiment_fig_with_line'
+            gfp_div_config.det = 'data/white_cropped'
             executer = Executer(gfp_div_config)
             executer.execute()
             if crop:
@@ -64,10 +78,11 @@ def create_exp_figs(crop=False):
     esp_thresh_config = Config(get_config())
     esp_thresh = [0, 1, 2, 3, 4, 5, 6, 10, 20]
     for dst in esp_thresh:
-        esp_thresh_config.output = 'esp_thresh_{}'.format(dst)
-        esp_thresh_config.edge_mask = [0, dst, 120]
+        esp_thresh_config.output = 'esp_thresh_corner_enhanced_3_{}'.format(dst)
+        esp_thresh_config.edge_mask = [1, dst, 120]
         esp_thresh_config.prepad_thresh = -4
-        esp_thresh_config.det = 'data/experiment_fig_with_line'
+        esp_thresh_config.det = 'data/white_cropped'
+        esp_thresh_config.enhance = 3
         executer = Executer(esp_thresh_config)
         executer.execute()
         if crop:
@@ -75,17 +90,19 @@ def create_exp_figs(crop=False):
                     os.path.join(os.getcwd(), esp_thresh_config.det, 
                         'out_{}_{}'.format(esp_thresh_config.output, 
                             esp_thresh_config.image.split('/')[-1])),
-                    'left',
+                    'bottom_left',
                     200
                     )
 
     gfp_thresh_config = Config(get_config())
     gfp_thresh = [80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 200, 250, 300]
     for size in gfp_thresh:
-        gfp_thresh_config.output = 'gfp_thresh_{}'.format(size)
+        gfp_thresh_config.output = 'gfp_thresh_enhanced_{}'.format(size)
         gfp_thresh_config.center_mask = size
         gfp_thresh_config.large_thresh = 120000
-        gfp_thresh_config.det = 'data/experiment_fig_with_line'
+        gfp_thresh_config.det = 'data/white_cropped'
+        gfp_thresh_config.enhance = 4
+        executer = Executer(esp_thresh_config)
         executer = Executer(gfp_thresh_config)
         executer.execute()
         if crop:
@@ -104,10 +121,10 @@ def get_config():
             'video': '', # os.path.join(os.getcwd(), 'data/videos/half_inter10_noon.avi'),
 
             # The input image, when you apply to an image.
-            'image': os.path.join(os.getcwd(), 'data/images/ex04.png'),
+            'image': os.path.join(os.getcwd(), 'data/images/white.png'),
             'det': 'data/images',
 
-            'output': 'boxline',
+            'output': '0',
 
             # THe experiment for the PMD
             'pmd_div_num': 9, # choose in [4, 9, 16]
@@ -122,7 +139,11 @@ def get_config():
             'edge_mask': [],
 
             # The size of the mask you create
-            'center_mask': 300,
+            'center_mask': 0,
+
+            # The enhance color for experiment figs
+            # the factor multiplied to image
+            'enhance': 0,
 
             # For network configuration
             'segmentation': False,
@@ -166,7 +187,7 @@ def get_config():
             # For design of the output image or video
 
             # Write the bouding box at the reconstruction part
-            'boxline': 1,
+            'boxline': 0,
 
             'concat_all': False,
 
@@ -209,9 +230,10 @@ def get_config():
     return config
 
 if __name__ == '__main__':
-    create_exp_figs(crop=True)
     # main()
     # v2f()
     # evaluate()
+    # create_exp_figs(crop=True)
+    execute_to_dir()
 
 
